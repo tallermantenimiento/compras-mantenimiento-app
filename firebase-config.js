023ce0c2156b58b03a61f154045fc1657b512a7e
tallermantenimiento <taller.mantenimiento@sklum.com>
2025-12-29 07:29:38 +0100

    Fix firebase-config for GitHub Pages

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
-// Firebase (SDK modular por URL) - Compatible con GitHub Pages
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
+// firebase-config.js (versión CDN para GitHub Pages)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
 
-const firebaseConfig = {
+// Config REAL de tu proyecto
+export const firebaseConfig = {
   apiKey: "AIzaSyBVCaP1DJqkt-kbPU9xUvKI-YgXIB32UF8",
   authDomain: "compras-mantenimiento-app.firebaseapp.com",
   projectId: "compras-mantenimiento-app",
@@ -10,9 +11,15 @@ const firebaseConfig = {
   messagingSenderId: "421706450966",
   appId: "1:421706450966:web:38ed25089d8c883079cd37",
 
-  // MUY IMPORTANTE (Realtime Database)
+  // URL de Realtime Database (IMPORTANTE)
   databaseURL: "https://compras-mantenimiento-app-default-rtdb.europe-west1.firebasedatabase.app"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+// Inicializa
+export const firebaseApp = initializeApp(firebaseConfig);
+export const db = getDatabase(firebaseApp);
+
+// Para probar en consola
+window.firebaseApp = firebaseApp;
+window.firebaseConfig = firebaseConfig;
+window.db = db;
